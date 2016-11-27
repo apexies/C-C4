887dba21acb1c099daef86d7fea773c6b558d8ed
apexies <apexies@gmail.com>
2016-11-27 17:03:03 -0500

    Update sketch.js

--- P5.JS LoungeLoudness/sketch.js
+++ P5.JS LoungeLoudness/sketch.js
@@ -3,6 +3,7 @@ var sensorVal1;
 
 var aiokey = "117d6eda7a35489ba5fd780ded9df06b";
 var feedName = "Microphone";
+
 function setup() {
   
 createCanvas(600,600);
@@ -15,15 +16,13 @@ background(255);
 textSize(25);
 textFont("Helvetica");
   text("How loud is the DF Lounge right now?", 10, 30) 
-  //fill(255,0,0);
- noStroke();
- ellipse(canvas.width/4,canvas.height/4,sensorVal1/2,sensorVal1/2);
+noStroke();
+ellipse(canvas.width/4,canvas.height/4,sensorVal1/2,sensorVal1/2);
 if (sensorVal1>500){
   fill(255,0,0);
   textSize(25);
   textFont("Helvetica");
   text("Noisy", width/2, 70);
-//  fill(255,0,0);
   }
 else if (sensorVal1>200){
   fill(244,155,66);
@@ -45,9 +44,6 @@ function receivedData(data)
 sensorVal1 = parseInt(data.value); 
 println(sensorVal1);
 }
-
-
-
 
 function askAIO()
 {
